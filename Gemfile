07fe2893819b091bf01730a20e8f7a680a71c64f
Steve Praski <steven_praski@acl.com>
2014-05-22 23:12:27 -0700

    first screenshot poc

--- Gemfile
+++ Gemfile
@@ -1,10 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'require_all'
-
 gem 'rspec'
 gem 'capybara'
 gem 'selenium-webdriver'
-
-# gem 'poltergeist'
-# gem 'rake'
--- nawc.rb
+++ nawc.rb
@@ -12,6 +12,7 @@ require 'capybara/rspec'
 
 require 'pry'
 
+
 config_yml = YAML.load_file('config.yml')
 raise "Empty config.yml" if not config_yml
 
@@ -37,4 +38,45 @@ if defined? login()
   visit TestConfig["login"]["url"]
   login(TestConfig["login"]["username"], TestConfig["login"]["password"])
 end
+
+# hard coded for now
+site_links = Array.new
+link_file = File.open("sites/links", "r")
+# FIXME: this should be an array of hashes
+link_file.each_line do |line|
+  site_links << line.split('|')
+end
+
+def wait_until_url (url)
+  timeout = 60
+  timeout.times do
+    return if current_url == url
+    sleep 1
+  end
+  raise "URL: #{url} did not load!"
+end
+
+def wait_until(proc)
+  timeout = 60
+  timeout.times do
+    return if proc.call
+    sleep 1
+  end
+end
+
+
+# also harded for now
+# also need to make the page search text optional (and/or as an xpath or object)
+
+visit site_links[0][0]
+# because waiting for page load is unimportant to Capybara
+wait_until_url(site_links[0][0])
+
+# find target text (e.g., some page content)
+wait_until(Proc.new { page.has_text?(site_links[0][1]) } )
+
+# do not find target text (e.g., page load indicator)
+wait_until(Proc.new { page.has_no_text?(site_links[0][2]) } )
+
+page.save_screenshot('sites/Screenshot 0.png')
 
